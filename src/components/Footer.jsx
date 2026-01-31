22afa5d340eae87de81048457f4ec542dc128588
nidhi-ai01 <nidhitiwari.feb01@gmail.com>
2026-01-31 16:34:05 +0530

    Changed the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,6 @@ const Footer = ({ logo }) => {
 
   return (
     <footer className="bg-slate-950 text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-14">
 
         {/* Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
